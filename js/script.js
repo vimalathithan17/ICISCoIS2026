4ff6556f27d6e0b2cf74000ba35801639597634b
vimalathithan17 <vimalathithan17@gmail.com>
2024-01-17 12:20:03 +0000

    changes made

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
-const navigation_height=document.getElementById('navigation').offsetHeight;
+const navigation_height = document.getElementById('navigation').offsetHeight;
 //console.log(navigation_height);
-document.documentElement.style.setProperty('--scroll-padding',navigation_height+'px');
-const menu=document.getElementById("menu");
+document.documentElement.style.setProperty('--scroll-padding', navigation_height + 'px');
+const menu = document.getElementById("menu");
 //console.log(menu);
-menu.addEventListener('click',(e)=>{
-    const navbar=document.getElementById('navbar');
-    navbar.style.display='grid';
-    let nav_items=document.getElementsByClassName('nave_item');
-    for(let i=0;i<nav_items.length;i++){
-        nav_items[i].addEventListener('click',(e)=>{
-            navbar.style.display='none';
+let menu_drop = false;
+menu.addEventListener('click', (e) => {
+    const navbar = document.getElementById('navbar');
+    if (!menu_drop) {
+        navbar.style.display = 'grid';
+        menu_drop = true;
+    }
+    else {
+        navbar.style.display = 'none';
+        menu_drop = false
+    }
+    let nav_items = document.getElementsByClassName('nave_item');
+    for (let i = 0; i < nav_items.length; i++) {
+        nav_items[i].addEventListener('click', (e) => {
+            navbar.style.display = 'none';
+            menu_drop = false;
         })
     }
 })
